50b699416916557cd4eefa87347a31c420779476
Zaetic <joaogabrielggs@gmail.com>
2021-01-01 13:40:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+<p align="center">
+    <img src="https://github.com/Zaetic/Twitch-Easy/blob/master/images/twitch_easy.png?raw=true" alt="twitch easy">
+</p>
+<p align="center">
+    <a href="https://github.com/airbnb/javascript"><img src="https://img.shields.io/badge/Code--style-Airnb-red?logo=Airbnb&style=flat-square"></a>
+    <a href="https://www.npmjs.com/package/twitch-easy"><img src="https://img.shields.io/npm/v/twitch-easy?color=blue&logo=npm&style=flat-square"></a>
+</p>
+
+Hey, do you want an easy connection to the Twitch API ?? I am here to solve this problem !!
+
+I am an opensouce wrapper that makes the connection with the twitch `Helix API` and i was built to be fast and very easy to use !! 
+
+## Installation
+
+To add Twitch Easy to your project, just execute:
+
+``` bash 
+$ npm i --save twitch
+```
+
+## Contents
+
+- [Getting Started](#getting-started)
+- [Functions](#functions)
+
+## Getting started
+
+### Calling
+``` js
+const TwitchEasy = require('twitch-easy');
+
+const api = new TwitchEasy('CLIENT_ID', 'CLIENT_SECRET')
+```
+It is necessary to pass a client_id and client_secret, to get this information acess: https://dev.twitch.tv/console
+
+### Example
+``` js
+const TwitchEasy = require('twitch-easy');
+
+const api = new TwitchEasy('CLIENT_ID', 'CLIENT_SECRET')
+
+async function getStreamer(){
+    const streamer = await api.getStreamers('streamer_name');
+    console.log(streamer);
+    return streamer;
+}
+
+getStreamer();
+```
+
+## Functions
+
+`getStreamers (name, qnt)` - Returns a array list referring to the searched streamer name
+
+`getStreamerById (id)` - Returns a streamer by id
---
+++ src/twitchAPI.ts
@@ -0,0 +1,87 @@
+import fetch from 'node-fetch';
+
+class TwitchAPI {
+    private CLIENT_ID: string;
+    private CLIENT_SECRET: string;
+    private twitchAouth2: string;
+    private token: { access_token: string; expires_in: number; time: number; token_type: string; };
+    private twitch: { GET_CHANNEL: string, GET_STREAM: string };
+
+    constructor(clientId: string, clientSecret: string) {
+        this.CLIENT_ID = clientId;
+        this.CLIENT_SECRET = clientSecret;
+        this.twitchAouth2 = 'https://id.twitch.tv/oauth2/token';
+        this.twitch = {
+            GET_CHANNEL: 'https://api.twitch.tv/helix/search/channels',
+            GET_STREAM: 'https://api.twitch.tv/helix/streams',
+        };
+        this.token = {
+            access_token: '',
+            expires_in: 0,
+            time: 0,
+            token_type: 'bearer',
+        };
+    }
+
+    private async getToken(): Promise<void> {
+        if (await this.checkToken() === false) return;
+        const body = {
+            client_id: this.CLIENT_ID,
+            client_secret: this.CLIENT_SECRET,
+            grant_type: 'client_credentials',
+        };
+
+        const token = await fetch(this.twitchAouth2, {
+            method: 'post',
+            body: JSON.stringify(body),
+            headers: { 'Content-type': 'application/json' },
+        }).then((res) => res.json());
+
+        this.token.access_token = token.access_token;
+        this.token.expires_in = token.expires_in;
+        this.token.token_type = token.token_type;
+        this.token.time = new Date().getTime();
+    }
+
+    private async checkToken(): Promise<Boolean> {
+        const errorMargin = 1000;
+        if ((this.token.time + this.token.expires_in + errorMargin) >= new Date().getTime()) {
+            return false;
+        }
+        return true;
+    }
+
+    public async getStreamers(name: string, quantity: number = 20) {
+        await this.getToken();
+        const url = `${this.twitch.GET_CHANNEL}?query=${name}&first=${quantity}`;
+
+        const streamers = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Client-ID': this.CLIENT_ID,
+                Authorization: `Bearer ${this.token.access_token}`,
+            },
+        }).then((res) => res.json());
+
+        return streamers;
+    }
+
+    public async getStreamerById(id: number) {
+        await this.getToken();
+        const url = `${this.twitch.GET_STREAM}?first=1&user_id=${id}`;
+
+        const streamer = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Client-ID': this.CLIENT_ID,
+                Authorization: `Bearer ${this.token.access_token}`,
+            },
+        }).then((res) => res.json());
+
+        return streamer.data[0];
+    }
+}
+
+module.exports = TwitchAPI;
