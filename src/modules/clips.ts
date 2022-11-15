b7aaa72f50ebc2d2285ec807aa80026bdb41ee46
Zaetic <joaogabrielggs@gmail.com>
2022-11-14 23:22:39 -0300

    feat: Clips module created

---
+++ src/modules/clips.ts
@@ -0,0 +1,123 @@
+import axios from 'axios';
+import { GET_CLIPS } from '../defaults';
+import { Clip, ClipsSearchOnline, ITwitchAPI } from '../types/twitchAPI';
+
+class Clips {
+    private core: ITwitchAPI;
+
+    constructor(TwitchAPI: ITwitchAPI) {
+        this.core = TwitchAPI;
+    }
+
+    public async getClips({
+        quantity = 20,
+        id,
+        gameId,
+        broadcasterId,
+    }: {
+        quantity?: number;
+        id?: string | Array<string>;
+        gameId?: string;
+        broadcasterId?: string;
+    }): Promise<Clip[] | null> {
+        if (!id && !gameId && !broadcasterId) throw new Error('id (one or more), broadcasterId, or gameId must be specified');
+
+        let cursor: string | null = null;
+        let clips: Clip[] = [];
+        let finish = false;
+        const objFetch: {
+            id?: string | Array<string>;
+            gameId?: string;
+            broadcasterId?: string;
+            quantity: number;
+        } = {
+            id: id || undefined,
+            gameId: gameId || undefined,
+            broadcasterId: broadcasterId || undefined,
+            quantity,
+        };
+
+        if (quantity < 100) {
+            const clipsFetch = await this.fetchClips(objFetch);
+            if (!clipsFetch) return null;
+            clips = clipsFetch.data;
+        } else {
+            while (finish === false) {
+                let clipsFetch: ClipsSearchOnline | null = null;
+                const left = quantity - clips.length;
+
+                if (left <= 0) {
+                    finish = true;
+                } else {
+                    const quantityLeft = left < 100 ? left : 100;
+                    objFetch.quantity = quantityLeft;
+
+                    if (!cursor) clipsFetch = await this.fetchClips(objFetch);
+                    else if (cursor) clipsFetch = await this.fetchClips(objFetch);
+
+                    if (!clipsFetch || clipsFetch.data.length === 0) finish = true;
+                    else {
+                        clips = Array.prototype.concat(clips, clipsFetch.data);
+
+                        if (clipsFetch.pagination.cursor) cursor = clipsFetch.pagination.cursor;
+                        else if (!clipsFetch.pagination.cursor) finish = true;
+                    }
+                }
+            }
+        }
+
+        return clips;
+    }
+
+    private async fetchClips({
+        quantity = 20,
+        id,
+        gameId,
+        broadcasterId,
+        paginator,
+    }: {
+        quantity: number;
+        id?: string | Array<string>;
+        gameId?: string;
+        broadcasterId?: string;
+        paginator?: string;
+    }): Promise<ClipsSearchOnline | null> {
+        await this.core.getToken();
+        const headers = this.core.createHeader();
+
+        let url = `${GET_CLIPS}?`;
+
+        if (id) url += `id=${id}`;
+        if (gameId) url += `game_id=${gameId}`;
+        if (broadcasterId) url += `broadcaster_id=${broadcasterId}`;
+
+        url += `&first=${quantity}`;
+        if (paginator) url += `&after=${paginator}`;
+
+        const games: ClipsSearchOnline = await axios({
+            url,
+            method: 'GET',
+            headers,
+        })
+            .then((res) => {
+                this.core.updateRateReset(res.headers['ratelimit-reset']);
+
+                if (res.status === 200) {
+                    return res.data;
+                }
+
+                if (res.status === 429) {
+                    throw new Error(`Excess rate limit, will be reset at ${this.core.ratelimit}`);
+                }
+
+                return null;
+            })
+            .catch((err: Error) => {
+                throw err;
+            });
+
+        return games;
+    }
+}
+
+export { Clips };
