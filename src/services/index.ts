a3520163e0a7afe23e152d90ca805faa506ef968
Zaetic <joaogabrielggs@gmail.com>
2022-12-01 12:56:33 -0300

    test: Clips tests created

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,2 @@
 export * from './auth/auth';
 export * from './http/http';
-export * from './auth/auth.memory';
-export * from './http/http.memory';
---
+++ tests/unit/modules/clips.test.ts
@@ -0,0 +1,144 @@
+import { Clips } from '../../../src/modules';
+import { AuthMemory } from '../../../src/services/auth/auth.memory';
+import { HttpMemory } from '../../../src/services/http/http.memory';
+import { IAuth } from '../../../src/services/auth/auth.declaration';
+import { IHttp } from '../../../src/services/http/http.declaration';
+
+describe('Clips', () => {
+    const dataMock = [
+        {
+            id: 'DeliciousDelightfulPicklesWOOP',
+            url: 'https://clips.twitch.tv/DeliciousDelightfulPicklesWOOP',
+            embed_url: 'https://clips.twitch.tv/embed?clip=DeliciousDelightfulPicklesWOOP',
+            broadcaster_id: '10000000',
+            broadcaster_name: 'tester',
+            creator_id: '20000000',
+            creator_name: 'test_creator',
+            video_id: '300000000',
+            game_id: '65876',
+            language: 'en',
+            title: 'Makes the wrong choice',
+            view_count: 759492,
+            created_at: '2020-12-09T19:18:10Z',
+            thumbnail_url: 'https://clips-media-assets2.twitch.tv/AT-cm%7C100000000-preview-480x272.jpg',
+            duration: 44.9,
+            vod_offset: null,
+        },
+        {
+            id: 'AnnoyingDepressedEmuKappaClaus',
+            url: 'https://clips.twitch.tv/AnnoyingDepressedEmuKappaClaus',
+            embed_url: 'https://clips.twitch.tv/embed?clip=AnnoyingDepressedEmuKappaClaus',
+            broadcaster_id: '10000001',
+            broadcaster_name: 'tester1',
+            creator_id: '20000001',
+            creator_name: 'test_creator1',
+            video_id: '400000000',
+            game_id: '65876',
+            language: 'en',
+            title: 'Cyberpunk 2077 driving',
+            view_count: 662569,
+            created_at: '2018-08-27T17:26:43Z',
+            thumbnail_url: 'https://clips-media-assets2.twitch.tv/AT-cm%7C200000000-preview-480x272.jpg',
+            duration: 44,
+            vod_offset: 33977,
+        },
+    ];
+
+    it('[getClips] Should call getClips and return a array of clips', async () => {
+        const _http: IHttp = new HttpMemory();
+        const mock = {
+            status: 200,
+            statusText: 'OK',
+            headers: {
+                'content-type': 'application/json; charset=utf-8',
+                'ratelimit-limit': '800',
+                'ratelimit-remaining': '799',
+                'ratelimit-reset': '1669809433',
+                'timing-allow-origin': 'https://www.twitch.tv',
+            },
+            data: { data: dataMock },
+        };
+
+        _http.get = jest.fn().mockImplementationOnce(() => Promise.resolve(mock));
+
+        const _auth: IAuth = new AuthMemory(_http, '', '');
+        const clips = new Clips(_http, _auth);
+
+        const findClips = await clips.getClips({ gameId: '65876' });
+        expect(findClips).not.toBeNaN();
+        expect(findClips).not.toBeNull();
+        expect(findClips).toHaveLength(2);
+        expect(findClips).toEqual(dataMock);
+    });
+
+    it('[getClips] Should call getClips and return a rate limit error', async () => {
+        const _http: IHttp = new HttpMemory();
+        const mock = {
+            status: 429,
+            statusText: 'OK',
+            headers: {
+                'content-type': 'application/json; charset=utf-8',
+                'ratelimit-limit': '800',
+                'ratelimit-remaining': '0',
+                'ratelimit-reset': '1669809433',
+                'timing-allow-origin': 'https://www.twitch.tv',
+            },
+            data: { data: dataMock },
+        };
+
+        _http.get = jest.fn().mockImplementationOnce(() => Promise.resolve(mock));
+
+        const _auth: IAuth = new AuthMemory(_http, '', '');
+        const clips = new Clips(_http, _auth);
+
+        await expect(clips.getClips({ gameId: '65876' })).rejects.toThrow('Excess rate limit, will be reset at');
+    });
+
+    it('[getClips] Should call getClips and return a unexpected error', async () => {
+        const _http: IHttp = new HttpMemory();
+        const mock = {
+            status: 500,
+            statusText: 'OK',
+            headers: {
+                'content-type': 'application/json; charset=utf-8',
+                'ratelimit-limit': '800',
+                'ratelimit-remaining': '20',
+                'ratelimit-reset': '1669809433',
+                'timing-allow-origin': 'https://www.twitch.tv',
+            },
+        };
+
+        _http.get = jest.fn().mockImplementationOnce(() => Promise.resolve(mock));
+
+        const _auth: IAuth = new AuthMemory(_http, '', '');
+        const clips = new Clips(_http, _auth);
+
+        const findClips = await clips.getClips({ gameId: '65876' });
+        expect(findClips).not.toEqual(dataMock);
+        expect(findClips).not.toBeNaN();
+        expect(findClips).toBeNull();
+    });
+
+    it('[getClips] Should call getClips without params and return a throw', async () => {
+        const _http: IHttp = new HttpMemory();
+        const mock = {
+            status: 200,
+            statusText: 'OK',
+            headers: {
+                'content-type': 'application/json; charset=utf-8',
+                'ratelimit-limit': '800',
+                'ratelimit-remaining': '799',
+                'ratelimit-reset': '1669809433',
+                'timing-allow-origin': 'https://www.twitch.tv',
+            },
+            data: { data: dataMock },
+        };
+
+        _http.get = jest.fn().mockImplementationOnce(() => Promise.resolve(mock));
+
+        const _auth: IAuth = new AuthMemory(_http, '', '');
+        const clips = new Clips(_http, _auth);
+
+        await expect(clips.getClips({})).rejects.toThrow('id (one or more), broadcasterId, or gameId must be specified');
+    });
+});
