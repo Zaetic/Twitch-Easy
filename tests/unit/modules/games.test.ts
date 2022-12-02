6eb68fb1d1336f119506014df1fdd7799a721c4b
Zaetic <joaogabrielggs@gmail.com>
2022-12-02 18:43:23 -0300

    test: GetGameByName tests created to games

--- tests/unit/modules/games.test.ts
+++ tests/unit/modules/games.test.ts
@@ -38,6 +38,15 @@ describe('Games', () => {
         },
     ];
 
+    const dataMockName = [
+        {
+            id: '65876',
+            name: 'Cyberpunk 2077',
+            box_art_url: 'https://static-cdn.jtvnw.net/ttv-boxart/65876_IGDB-{width}x{height}.jpg',
+            igdb_id: '1877',
+        },
+    ];
+
     it('[getTopGames] Should call getTopGames and return a array of 5 top games', async () => {
         const _http: IHttp = new HttpMemory();
         const mock = {
@@ -120,5 +129,89 @@ describe('Games', () => {
         const games = new Games(_http, _auth);
 
         await expect(games.getTopGames(0)).rejects.toThrow('The parameter "quantity" was malformed: the value must be greater than or equal to 1');
+    });
+
+    it('[getGameByName] Should call getGameByName and return a game by name', async () => {
+        const _http: IHttp = new HttpMemory();
+        const mock = {
+            status: 200,
+            statusText: 'OK',
+            headers: {
+                'content-type': 'application/json; charset=utf-8',
+                'ratelimit-limit': '800',
+                'ratelimit-remaining': '799',
+                'ratelimit-reset': '1670004777',
+                'timing-allow-origin': 'https://www.twitch.tv',
+            },
+            data: { data: dataMockName },
+        };
+
+        _http.get = jest.fn().mockImplementationOnce(() => Promise.resolve(mock));
+
+        const _auth: IAuth = new AuthMemory(_http, '', '');
+        const games = new Games(_http, _auth);
+
+        const findGame = await games.getGameByName('cyberpunk 2077');
+        expect(findGame).not.toBeNaN();
+        expect(findGame).not.toBeNull();
+        expect(typeof findGame).toBe('object');
+        expect(findGame).toEqual(dataMockName[0]);
+    });
+
+    it('[getGameByName] Should call getGameByName and return a rate limit error', async () => {
+        const _http: IHttp = new HttpMemory();
+        const mock = {
+            status: 429,
+            statusText: 'OK',
+            headers: {
+                'content-type': 'application/json; charset=utf-8',
+                'ratelimit-limit': '800',
+                'ratelimit-remaining': '0',
+                'ratelimit-reset': '1669809433',
+                'timing-allow-origin': 'https://www.twitch.tv',
+            },
+            data: {},
+        };
+
+        _http.get = jest.fn().mockImplementationOnce(() => Promise.resolve(mock));
+
+        const _auth: IAuth = new AuthMemory(_http, '', '');
+        const games = new Games(_http, _auth);
+
+        await expect(games.getGameByName('cyberpunk 2077')).rejects.toThrow('Excess rate limit, will be reset at');
+    });
+
+    it('[getGameByName] Should call getGameByName and return a unexpected error', async () => {
+        const _http: IHttp = new HttpMemory();
+        const mock = {
+            status: 500,
+            statusText: 'OK',
+            headers: {
+                'content-type': 'application/json; charset=utf-8',
+                'ratelimit-limit': '800',
+                'ratelimit-remaining': '20',
+                'ratelimit-reset': '1669809433',
+                'timing-allow-origin': 'https://www.twitch.tv',
+            },
+        };
+
+        _http.get = jest.fn().mockImplementationOnce(() => Promise.resolve(mock));
+
+        const _auth: IAuth = new AuthMemory(_http, '', '');
+        const games = new Games(_http, _auth);
+
+        const findGame = await games.getGameByName('cyberpunk 2077');
+        expect(findGame).not.toEqual(dataMockName[0]);
+        expect(findGame).not.toBeNaN();
+        expect(findGame).toBeNull();
+    });
+
+    it(`[getGameByName] Should call getGameByName with a string equal '' or null and return a throw`, async () => {
+        const _http: IHttp = new HttpMemory();
+
+        const _auth: IAuth = new AuthMemory(_http, '', '');
+        const games = new Games(_http, _auth);
+
+        await expect(games.getGameByName('')).rejects.toThrow('Name is null, pass a value');
     });
 });
