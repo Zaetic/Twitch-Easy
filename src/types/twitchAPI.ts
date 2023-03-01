34232fc48589ba78679729eb7442b53bb3b120af
Zaetic <joaogabrielggs@gmail.com>
2023-02-28 21:41:46 -0300

    feat: Add description to interface core

--- src/types/twitchAPI.ts
+++ src/types/twitchAPI.ts
@@ -1,7 +1,16 @@
 import { Clips, Games, Streamers } from '../modules';
 
 export interface ITwitchAPI {
+    /**
+     * Get games module
+     */
     get games(): Games;
+    /**
+     * Get streamers module
+     */
     get streamers(): Streamers;
+    /**
+     * Get clips module
+     */
     get clips(): Clips;
 }
